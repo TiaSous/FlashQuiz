bf9074f55ef76674a8dcd03917aa463b67b09b02
TiaSous <tiagorodriguessousa07@gmail.com>
2024-04-26 12:12:32 +0200

    change page modif

--- FlashQuiz/Modifier.xaml.cs
+++ FlashQuiz/Modifier.xaml.cs
@@ -5,5 +5,20 @@ public partial class Modifier : ContentPage
 	public Modifier()
 	{
 		InitializeComponent();
-	}
+
+        var cards = new List<Card>
+            {
+            new Card { Terme = "avoir", Definition = "haben" },
+            new Card { Terme = "l'objet", Definition = "der Gegenstand, -¨e" },
+            new Card { Terme = "Salut", Definition = "hallo" },
+            };
+
+        cardsListView.ItemsSource = cards;
+    }
+}
+
+public class Card
+{
+    public string Terme { get; set; }
+    public string Definition { get; set; }
 }
